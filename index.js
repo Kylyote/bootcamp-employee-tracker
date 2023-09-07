95baae4b77301bc847bb01e7c8069187f8f9b3ad
Kyle Jocoy <kjocoy@gmail.com>
2023-09-06 22:02:25 -0700

    finshed the main code and it works, but there is a race scenario in the updateEmployee function.

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function viewRoles() {
   })
 }
 function viewEmployees() {
-  db.query("SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees JOIN roles ON employees.title = roles.id JOIN departments ON roles.department = departments.id LEFT JOIN employees AS manager ON employees.manager = manager.id;", function(err, results) { 
+  db.query("SELECT employees.id, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', roles.title AS 'Job Title', departments.department_name AS Department, roles.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employees JOIN roles ON employees.title = roles.id JOIN departments ON roles.department = departments.id LEFT JOIN employees AS manager ON employees.manager = manager.id;", function(err, results) { 
     if (err) console.log(err);
     console.table(results);
     startMenu();
@@ -89,8 +89,8 @@ function addDepart() {
     }
   ])
   .then((response) => {
-    db.query('INSERT INTO departments VALUES ?', response.departName, (err, result) => {
-      if(err) console.log(err);
+    db.query('INSERT INTO departments VALUES ?;', response.departName, (err, result) => {
+      if(err) console.log(err); 
       console.log('Department Result', result);
     });
   });
@@ -99,52 +99,173 @@ function addDepart() {
 
 // Function that will take user input and add a new role to the roles table, then it will call viewRoles() to show the changes, then go back to the startMenu().
 function addRole (){
-  db.query('SELECT departments.id, departments.department_name FROM departments', (err, res) => {
+  let departList = [];
+  db.query('SELECT departments.id, departments.department_name FROM departments;', (err, res) => {
     if (err) throw err;
-    let departList = [];
     // Store departments from query into an array
     res.forEach((department) => {
+      // Adding name and value in like this makes inquirer show the name but save the value. This works since MySQL is expecting a value but people read things as names.
       departList.push({
         name: department.department_name,
         value: department.id,
       });
     });
-      // Ask the user some questions.
-    inquirer.prompt([
-      {
-        type: 'input',
-        message: 'What is the role you want to add?',
-        name: 'roleName'
-      },
-      {
-        type: 'input',
-        message: 'What is the yearly salary for this role?',
-        name: 'roleSalary'
-      },
-      {
-        type: 'list',
-        message: 'What department is this in? ',
-        choices: departList,
-        name: 'departID'
-      },
-    ]).then((response) => {
-      console.log(response);
-      db.query('INSERT INTO roles (title, department, salary) VALUES (?, ?, ?)', [response.roleName, response.departID, response.roleSalary], (err, result) => {
-        if (err) console.log(err);
-        console.log("Role Result", result);
-      });
-      viewRoles();
-    })
+  });
+  // Ask the user some questions. departList is added in so that the proper list of departments is shown. 
+  departList.push('New Department');
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the role you want to add?',
+      name: 'roleName'
+    },
+    {
+      type: 'input',
+      message: 'What is the yearly salary for this role?',
+      name: 'roleSalary'
+    },
+    {
+      type: 'list',
+      message: 'What department is this in? ',
+      choices: departList,
+      name: 'departID'
+    }
+  ])
+  .then((response) => {
+    console.log(response);
+    db.query('INSERT INTO roles (title, department, salary) VALUES (?, ?, ?);', [response.roleName, response.departID, response.roleSalary], (err, result) => {
+      if (err) console.log(err);
+      console.log("Role Result", result);
     });
-
+    viewRoles();
+  })
 }
 
 // Function that will take user input and create a new employee with department and roles. 
 function addEmployee(){
+  // Make array with names and values for the new employee department.
+  let departList = [];
+  db.query('SELECT departments.id, departments.department_name FROM departments;', (err, res) => {
+    if (err) throw err;
+    // Store departments from query into an array
+    res.forEach((department) => {
+      // Adding name and value in like this makes inquirer show the name but save the value. This works since MySQL is expecting a value but people read things as names.
+      departList.push({
+        name: department.department_name,
+        value: department.id,
+      });
+    });
+  });
+  // Make array with names and values for the new employee role.
+  let roleList = [];
+  db.query('SELECT roles.id, roles.title FROM roles;', (err, res) => {
+    if (err) throw err;
+    // Store roles from query into an array
+    res.forEach((role) => {
+      // Adding name and value in like this makes inquirer show the name but save the value. This works since MySQL is expecting a value but people read things as names.
+      roleList.push({
+        name: role.title,
+        value: role.id,
+      });
+    });
+  });
+  // Make list of all managers
+  let manList = [];
+  db.query("SELECT employees.id, CONCAT(employees.first_name,' ', employees.last_name) AS manager FROM employees;", (err, res) => {
+    if (err) throw (err);
+    res.forEach((manager) => {
+      manList.push({
+        name: manager.manager,
+        value: manager.id
+      });
+    });
+  });
+  console.log(departList);
   inquirer.prompt([
     {
-
+      type:'input',
+      message: 'What is the employee\'s first name?',
+      name: 'fName'
+    },
+    {
+      type: 'input',
+      message: 'What is the employee\'s last name?',
+      name: 'lName'
+    },
+    {
+      type: 'list',
+      message: 'What role does the new employee have?',
+      choices: roleList,
+      name: 'employRole'
+    },
+    {
+      type: 'list',
+      message: 'Who is the employee\'s manager?',
+      choices: manList,
+      name: 'employMan'
     }
-  ]);
+  ])
+  .then((response) => {
+    db.query('INSERT INTO employees (first_name, last_name, title, manager) VALUES (?, ?, ?, ?);',[response.fName, response.lName, response.employRole, response.employMan], (err, result) => {
+      if (err) console.log(err);
+      console.log('Employee Result', result);
+    });
+    viewEmployees();
+  }
+
+  );
+}
+
+// Change the role of an employee.
+function updateEmployee(){
+  // Show list of employees and their titles. 
+  let employList = [];
+  db.query("SELECT CONCAT(employees.first_name,' ',employees.last_name) as name, employees.id FROM employees;", (err, response) => {
+    if (err) throw (err);
+    response.forEach((employee) => {
+      employList.push({
+        name: employee.name,
+        value: employee.id
+      });
+    });
+  });
+
+  let roleList = [];
+  db.query('SELECT roles.id, roles.title FROM roles;', (err, res) => {
+    if (err) throw err;
+    // Store roles from query into an array
+    res.forEach((role) => {
+      // Adding name and value in like this makes inquirer show the name but save the value. This works since MySQL is expecting a value but people read things as names.
+      roleList.push({
+        name: role.title,
+        value: role.id,
+      });
+    });
+  });
+
+  // Need to add the Timeout function since the employList is not filled out in time. Users will not notice the tenth of a second delay. 
+  setTimeout(() => {
+    inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What employee needs to be updated?',
+      choices: employList,
+      name: 'employee'
+    },
+    {
+      type: 'list',
+      message: 'What the employee\'s new role?',
+      choices: roleList,
+      name: 'role'
+    }
+  ])
+  .then((response) => {
+    console.log('Info from Inquirer', response);
+    db.query('UPDATE employees SET title = ? WHERE id = ?;', [response.role, response.employee], (err, res) => {
+      if (err) throw err;
+      console.log('Employee Changed', res);
+    });
+    viewEmployees();
+  })}, 100);
 }
 startMenu();
